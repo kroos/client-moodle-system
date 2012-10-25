aa688d91a13b88d38b9e5555851cbb739c725634
dhiauddin@gmail.com <dhiauddin@gmail.com@f4e4f1c5-b0b2-f164-bc7e-17c070f2db28>
2012-10-25 04:13:36 +0000

    - fix all jquery issues

--- trunk/application/views/base_template.php
+++ trunk/application/views/base_template.php
@@ -6,26 +6,8 @@
 		<meta name="description" content="This is a System <?=$this->config->item('title')?> integrated with e-learning." />
 		<meta name="keywords" content="moodle, e-learning, learning online, web conference, islamic, islam" />
 		<meta name="author" content="Zaugola" />
-		<link rel="shortcut icon" href="<?=base_url()?>images/nightvision/favicon.ico" type="image/x-icon" />
+		<link rel="shortcut icon" href="<?=base_url()?>images/favicon.ico" type="image/x-icon" />
 		<link href="<?=base_url()?>css/jquery/jquery-ui-1.9.0.custom.css" rel="stylesheet">
-		<script src="<?=base_url()?>js/jquery/jquery-1.8.2.js"></script>
-		<script src="<?=base_url()?>js/jquery/jquery-ui-1.9.0.custom.js"></script>
-		<script>
-		$(function() {
-
-			$( "input[type=submit], a, button", ".demo" )
-				.button();
-		
-			$( "#radioset" ).buttonset();
-		});
-		</script>
-		<script type="text/javascript" src="<?=base_url()?>js/jquery/jquery-ui-timepicker-addon.js"></script>
-		<script type="text/javascript">
-			$(function(){
-				// Datepicker
-				$('#datepicker1').datetimepicker({dateFormat: "yy-mm-dd", timeFormat: "hh:mm:ss", showSecond: true, showMillisec: false, ampm: false, stepHour: 1, stepMinute: 1, stepSecond: 5});
-			});
-		</script>
 	<? endblock() ?>
 
 	<? startblock('top_nav') ?>
@@ -75,6 +57,25 @@
 		<p>You can view more free CSS3 web templates <a href="http://www.css3templates.co.uk">here</a>.</p>
 		<p>This template is a fully documented 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design. There is also a working PHP contact form on the contact page.</p>
 		<p><?=form_input(array('name' => 'tdate', 'value' => set_value('tdate'), 'id' => 'datepicker1', 'size' => '20', 'maxlength' => '20'))?></p>
+	<? endblock() ?>
+
+	<? startblock('jscript') ?>
+		<script src="<?=base_url()?>js1/jquery/jquery-1.8.2.js"></script>
+		<script src="<?=base_url()?>js/jquery/jquery-ui-1.9.0.custom.js"></script>
+		<script>
+			$(function() {
+				$( "input[type=submit], a, button", ".demo" )
+					.button();
+				$( "#radioset" ).buttonset();
+			});
+		</script>
+		<script type="text/javascript" src="<?=base_url()?>js/jquery/jquery-ui-timepicker-addon.js"></script>
+		<script type="text/javascript">
+			$(function(){
+				// Datepicker
+				$('#datepicker1').datetimepicker({dateFormat: "yy-mm-dd", timeFormat: "hh:mm:ss", showSecond: true, showMillisec: false, ampm: false, stepHour: 1, stepMinute: 1, stepSecond: 5});
+			});
+		</script>
 	<? endblock() ?>
 
 <? end_extend() ?>
