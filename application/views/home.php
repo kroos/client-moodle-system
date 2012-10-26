148c054540f3d5c6efcd39dfe8fc82bbb14336e4
dhiauddin@gmail.com <dhiauddin@gmail.com@f4e4f1c5-b0b2-f164-bc7e-17c070f2db28>
2012-10-26 04:58:05 +0000

    - completing home page

--- application/views/home.php
+++ application/views/home.php
@@ -2,6 +2,27 @@
 
 	<? startblock('content') ?>
 		<h1>Course List</h1>
+		<?$rq = $this->course->course()?>
+		<?if($rq->num_rows() < 1):?>
+			<p><b>No Course Have Been Created</b></p>
+		<?else:?>
+			<table style="width:100%; border-spacing:0;">
+				<tr>
+					<td>Course Code</td>
+					<td>Course</td>
+					<td>Description</td>
+					<td>Period</td>
+				</tr>
+				<?foreach($rq->result() as $t):?>
+					<tr>
+						<td><?=$t->code_course?></td>
+						<td><?=$t->course?></td>
+						<td><?=$t->description?></td>
+						<td><?=$t->week?></td>
+					</tr>
+				<?endforeach?>
+			</table>
+		<?endif?>
 	<? endblock() ?>
 
 <? end_extend() ?>
